448a2ac34b0ee9d04ccbe1bb385d3d3b22a932e4
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2021-03-12 19:01:50 -0400

    Check-in inicial do módulo WebApiFornecedor

---
+++ Controllers/FornecedoresController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using WebApiFornecedor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiFornecedor.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : ControllerBase
+    {
+        [HttpGet]
+        public List<Fornecedor> Get()
+        {
+            Fornecedor Fornecedores = new Fornecedor();
+            return Fornecedores.ListarFornecedores();
+        }
+    }
+}
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+ using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace WebApiFornecedor.Models
+{
+    public class Fornecedor
+    {
+        public int id { get; set; }
+        public string razaosocial { get; set; }
+        public string nomeempresa { get; set; }
+        public string ramoatividade { get; set; }
+
+
+        public List<Fornecedor> ListarFornecedores()
+        {
+            var dbFornecedores = AppDomain.CurrentDomain.BaseDirectory + @"base.json";
+            var json = File.ReadAllText(dbFornecedores);
+            var listaFornecedores = JsonConvert.DeserializeObject<List<Fornecedor>>(json);
+
+            return listaFornecedores;
+
+        }
+
+        public bool ReescreverArquivo(List<Fornecedor> listaFornecedores)
+        {
+            var dbFornecedores = AppDomain.CurrentDomain.BaseDirectory + @"base.json";
+            var json = JsonConvert.SerializeObject(listaFornecedores, Formatting.Indented);
+            File.WriteAllText(dbFornecedores, json);
+            return true;
+
+        }
+
+    }
+}
